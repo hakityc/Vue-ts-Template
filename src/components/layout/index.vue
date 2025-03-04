b0eea0271c371738737dc6e484d2a26b1763b991
Tang <>
2025-03-04 10:26:17 +0800

    chore: 初始化 layout 组件并格式化代码
    - 在 src/components/layout 目录下创建 index.vue 文件
    - 添加 layout 组件的基本结构和类型定义
    - 使用 Biome 格式化 biome.json 文件

---
+++ src/components/layout/index.vue
@@ -0,0 +1,31 @@
+<script lang='ts' setup>
+/** imports  */
+
+// biome-ignore lint/suspicious/noEmptyInterface: <explanation>
+interface Props {}
+// biome-ignore lint/suspicious/noEmptyInterface: <explanation>
+interface Emits {}
+
+/** props  */
+defineProps<Props>();
+
+/** emits  */
+defineEmits<Emits>();
+
+/** ref  */
+
+/** computed */
+
+/** hooks */
+
+/** methods  */
+
+/** watch  */
+
+/** lifecycle  */
+</script>
+<template>
+
+</template>
+
+<style scoped></style>
